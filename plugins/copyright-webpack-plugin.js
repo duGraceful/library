fd35487d64ee1ae2a3fd5c035048340e46fc3a2b
duyating <grace_du@yunquna.com>
2021-05-24 16:25:01 +0800

    fix: 自定义插件

--- plugins/copyright-webpack-plugin.js
+++ plugins/copyright-webpack-plugin.js
@@ -5,7 +5,17 @@ class CopyRightWebpackPlugin {
     // }
     // compiler  webapck的实例
     apply(compiler) {
-
+        compiler.hooks.emit.tapAsync('CopyRightWebpackPlugin',(compilation, cb) => {
+            compilation.assets['copyriught.txt'] = {
+                source: function() {
+                    return 'copy copy';
+                },
+                size: function() {
+                    return 21;
+                }
+            }
+            cb();
+        })
     }
 }
 
